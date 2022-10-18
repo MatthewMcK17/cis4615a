59d7a0c2c6eebbc8d57b79eb8a76fbe460d4e628
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:22:40 -0400

    Update R10_THI03_J.java

--- R10_THI03_J.java
+++ R10_THI03_J.java
@@ -1,10 +1,10 @@
 /*
-  THI03-J: Noncomplain Code
-   - Always invoke wait() and await() methods inside a loop (can cause DoS)
+  THI03-J: Compliant Code
+   - Invokes wait() and await() methods inside a loop
 */
 
 synchronized (object) {
-  if (<condition does not hold>) {
+  while (<condition does not hold>) {
     object.wait();
   }
   // Proceed when condition holds
