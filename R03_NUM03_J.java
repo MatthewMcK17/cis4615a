20d165032c241671b7ca465a5bd7bce54ebcb444
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:34:11 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,6 +1,6 @@
 /*
   NUM03-J: Noncompliant Code
-   - Using generic for reading integer data without considering its sign
+   - Use integer types that can fully represent the possible range of unsigned data
 */
 
 public static int getInteger(DataInputStream is) throws IOException {
