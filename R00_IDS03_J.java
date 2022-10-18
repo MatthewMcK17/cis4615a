554092d9585db6320351d41dd06db330c54fedf9
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:14:34 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,10 +1,16 @@
 /* 
-  IDS03-J: Noncompliant Code
-   - Vulnerable to log injection
+  IDS03-J: Compliant Code
+   - Not vulnerable to log injection
 */
 
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-  logger.severe("User login failed for: " + username);
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
 }
