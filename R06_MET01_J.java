4cfdb71592095cf7f7e1c68cd52a9d8b27d8798f
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:29:20 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,9 +1,15 @@
 /*
   MET01-J: Noncompliant Code
-   - Uses assertions to validate arguments of public methods
+   - Uses assertions to validate arguments of public methods (doesn't get called if assertions are disabled)
 */
-
 public static int getAbsAdd(int x, int y) {
-  return Math.abs(x) + Math.abs(y);
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
+  }
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
+  return absX + absY;
 }
-getAbsAdd(Integer.MIN_VALUE, 1);
