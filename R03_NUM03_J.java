177c429f4279d8430cbaf0a44cf5badfe37098ca
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:18:55 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,8 +1,7 @@
 /*
-  NUM03-J: Noncompliant Code
-   - Using generic for reading integer data without considering its sign
+  NUM03-J: Compliant Code
 */
 
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
