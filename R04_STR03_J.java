5b84f2f94dacf6c324ca4a5b8a1d721700cc6a8e
Matt M <51388006+MatthewMcK17@users.noreply.github.com>
2022-10-17 23:19:35 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,11 +1,9 @@
 /*
-  STR03-J: Noncompliant Code
-   - Encodes noncharacter data as a string
+  STR03-J: Compliant Code
 */
 
-
 BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+String s = x.toString();  // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray); 
+x = new BigInteger(ns);
